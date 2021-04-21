d9595a1818cab8f183f29f900892c060f3dbdc09
EmreKeskin47 <43786018+EmreKeskin47@users.noreply.github.com>
2021-04-21 23:37:33 +0300

    home page implemented

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,9 +1,26 @@
+import { Kurum } from './../../models/kurum';
 import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  institutionInfo: Kurum;
+
   navigateToProfile() {}
+
+  ngOnInit() {
+    this.institutionInfo = new Kurum(
+      'bil wash laundry',
+      'Efe Çobanoğlu',
+      1,
+      5,
+      10
+    );
+  }
+
+  navigateToNewOrder() {}
+  navigateToQueue() {}
+  navigateToOrderList() {}
 }
